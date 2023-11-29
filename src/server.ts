190c151f56678ae1e738a0ce4ab4c238ecf25ba5
Catskhi <catskhi.studios@gmail.com>
2023-11-29 03:37:00 -0300

    feat: Adicionadas novas rotas para o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import express, { type Request, type Response } from 'express'
+import connect from '../config/db'
+import userRouter from './routes/users.routes'
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
+app.use('/', userRouter)
 app.get('/', function (req: Request, res: Response) {
   return res.send({
-    message: 'Uepa'
+    message: 'Bem-vindo à API.'
   })
 })
 
 app.listen(port, () => {
+  connect()
+    .catch((error) => {
+      console.log(error)
+    })
   console.log(`App listening on port: http://localhost:${port}/`)
 })
