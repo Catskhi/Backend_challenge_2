3f64b376324878222e06fdd15da8c9820ea2ff04
Catskhi <catskhi.studios@gmail.com>
2023-11-30 00:37:42 -0300

    fix: Mensagem de boas-vindas alterada para texto novamente.

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 })
 
 app.get('/', function (req: Request, res: Response) {
-  return res.send({
-    mensagem: 'Bem-vindo à API'
-  })
+  return res.send('Bem-vindo à API.')
 })
 
 app.listen(port, () => {
